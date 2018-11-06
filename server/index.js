8191c84f994fb5dc9df6f7530c26981db4c8ea62
Rachel Wong <rwrachelwong@gmail.com>
2018-11-06 02:01:15 +0000

    Set the external font and positioned the nav

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 // Basic express setup:
 
 const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
+const express       = require('express');
+const bodyParser    = require('body-parser');
 const app           = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,7 +20,7 @@ const db = require("./lib/in-memory-db");
 //
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+const DataHelpers = require('./lib/data-helpers.js')(db);
 
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
